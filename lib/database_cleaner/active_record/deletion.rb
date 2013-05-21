6f15aa7053fb471dc6e688a234259315610bfd46
Adam Davies <adzdavies@gmail.com>
2013-05-21 11:40:06 +0930

    Fix MySql2 complaining of invalid sql in delete strategy
    Changed to execute each delete individually

--- lib/database_cleaner/active_record/deletion.rb
+++ lib/database_cleaner/active_record/deletion.rb
@@ -10,11 +10,21 @@ module DatabaseCleaner::ActiveRecord
     def clean
       connection = connection_class.connection
       connection.disable_referential_integrity do
-        sql = tables_to_truncate(connection).map do |table_name|
-          "DELETE FROM #{connection.quote_table_name(table_name)}"
-        end.join(";")
+        
+        # HACK: MySql2 (for my project at least) seems to complain 
+        # about invalid SQL when submitting multiple statements (joined by ;)
+        #
+        # Changed here to execute individually... still 2x faster than truncate so worth it.
+        
+        #sql = tables_to_truncate(connection).map do |table_name|
+        #  "DELETE FROM #{connection.quote_table_name(table_name)}"
+        #end.join(";")
+        #
+        #connection.execute sql
 
-        connection.execute sql
+        tables_to_truncate(connection).each do |table_name|
+          connection.execute "DELETE FROM #{connection.quote_table_name(table_name)}"
+        end
       end
     end
 
